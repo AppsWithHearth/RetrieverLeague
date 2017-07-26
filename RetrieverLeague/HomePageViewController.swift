ff53d008af93c2f0e67c31b4bf6084212ea300f2
Uros Zivaljevic <uroszivaljevic19@gmail.com>
2017-07-27 00:43:57 +0200

    home page fix

--- RetrieverLeague/HomePageViewController.swift
+++ RetrieverLeague/HomePageViewController.swift
@@ -9,27 +9,35 @@
 import UIKit
 
 class HomePageViewController: BaseViewController {
-
+    
+    
+    let urlStr = "http://appswithheart.de/"
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         self.titleImage = #imageLiteral(resourceName: "icon_homepage")
+        if let url = URL(string: urlStr) {
+            let request = URLRequest(url: url)
+            webView.loadRequest(request)
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
